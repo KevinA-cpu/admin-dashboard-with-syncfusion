baf2f1723f630ee90c10f6c4a19e98ce3e79aa32
KevinA-cpu <kevinanderson1410@gmail.com>
2023-04-07 11:30:18 +0700

    #creating jsx files

---
+++ src/components/index.jsx
@@ -0,0 +1,19 @@
+// This file is used to export all the components from the components folder
+// This is done to make it easier to import components from the components folder
+// Charts
+export { default as LineChart } from "./Charts/LineChart";
+export { default as PieChart } from "./Charts/PieChart";
+export { default as SparkLineChart } from "./Charts/SparkLineChart";
+export { default as StackedChart } from "./Charts/StackedChart";
+// Others
+export { default as Button } from "./Others/Button";
+export { default as Cart } from "./Others/Cart";
+export { default as ChartsHeader } from "./Others/ChartsHeader";
+export { default as Chat } from "./Others/Chat";
+export { default as Footer } from "./Others/Footer";
+export { default as Header } from "./Others/Header";
+export { default as NavBar } from "./Others/NavBar";
+export { default as Notification } from "./Others/Notification";
+export { default as SideBar } from "./Others/SideBar";
+export { default as ThemeSettings } from "./Others/ThemeSettings";
+export { default as UserProfile } from "./Others/UserProfile";
